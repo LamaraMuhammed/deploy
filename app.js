9ce1d4d25f41b7389188383f4aa02af3494afc8d
Lamara Muhammed <lamaramuhammed6@gmail.com>
2025-06-15 23:24:20 +0100

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+require("dotenv").config();
 const http = require("http");
 const express = require("express");
 const port = process.env.PORT || 3000;
@@ -14,22 +14,25 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/", express.static(__dirname + "/public"));
 
 // set views
-app.set("view", "./view");
+app.set("views", "./view");
 
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/api', (req, res) => {
-    res.status(200).json({
-        "name": "Ya Rasulallah",
-        a: 1, b: 2, c: 3, d: 4
-    });
-    res.end();
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    name: "Ya Rasulallah",
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+  });
+  res.end();
 });
 
 server.listen(port, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000", port);
 });
