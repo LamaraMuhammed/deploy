8c20d0b9a178c27bbd288e05ef35cbdd1ddc612c
Lamara Muhammed <lamaramuhammed6@gmail.com>
2025-06-16 00:37:10 +0100

    gitignore file unnecessary ignoring fix

---
+++ connection/dbConnection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2');
+const mongoose = require("mongoose");
+
+
+// const connection = mysql.createConnection(JSON.parse(process.env.SQL));
+
+// connection.connect((err) => {
+//     if (err) {
+//         return "DB Connecting Error";
+//     }
+// });
+
+// mongoose db connection
+mongoose.connect(process.env.MONGODB_URL)
+    .then( (conn) => console.log('Mongoose connection'))
+    .catch(err => console.error("ERROR: ", err));
+
+
+module.exports = {
+    // connection,
+    mysql
+}
